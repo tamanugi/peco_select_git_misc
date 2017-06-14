5a33e464e184a2c7ed8b6a355535b01c0bb48e0b
tamanugi <tamanugi@gmail.com>
2017-06-14 22:56:28 +0900

    add peco_select_git_hash.fish

---
+++ functions/peco_select_git_hash.fish
@@ -0,0 +1,4 @@
+function peco_select_git_hash
+  git log --full-history --date=format:'%Y/%m/%d %H:%M:%S' --pretty=format:'%h [%ad] %an : %s' | peco | awk '{print $1}' | read foo
+  commandline -a $foo
+end
