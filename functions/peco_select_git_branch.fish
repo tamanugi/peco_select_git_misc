244b2835dc05929e4751a614ffa9ace9105aceb7
tamanugi <tamanugi@gmail.com>
2017-06-19 11:29:59 +0900

    fix

--- functions/peco_select_git_branch.fish
+++ functions/peco_select_git_branch.fish
@@ -1,7 +1,7 @@
 function peco_select_git_branch
   git branch -a |  tr -d ' ' | tr -d '*' | sed -e 's|^remotes/||g' | peco | awk '{print $1}' | read foo
 
-  if [$foo]
+  if [ $foo ]
     commandline -a $foo
   end
 end
